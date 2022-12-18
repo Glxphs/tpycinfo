814b006cb23af02169a672e667997caca9300311
Glxphs <glxphs2@gmail.com>
2022-12-18 22:01:04 +0800

    update Navitem.svelte

--- src/lib/Navitem.svelte
+++ src/lib/Navitem.svelte
@@ -4,7 +4,7 @@
   $: active = $page.url.pathname === href
 </script>
 
-<li class="nav-item">
+<li class="nav-item mx-3">
   <a class="nav-link" class:active aria-current="page" {href}>
     <slot />
   </a>
