807be24e4d141f57288d7331081989fd5f65ed60
Glxphs <glxphs2@gmail.com>
2023-03-12 17:47:32 +0800

    remove initial poem

--- src/routes/dictation/+page.svelte
+++ src/routes/dictation/+page.svelte
@@ -4,11 +4,7 @@
 
     $navtitle = "Chinese Dictation"
 
-    let poem = "《山居秋暝》王維\n" +
-        "空山新雨後，天氣晚來秋。\n" +
-        "明月松間照，清泉石上流。\n" +
-        "竹喧歸浣女，蓮動下漁舟。\n" +
-        "隨意春芳歇，王孫自可留。"
+    let poem = ""
     let poemLines = []
     let answer = []
     let canInput = []
